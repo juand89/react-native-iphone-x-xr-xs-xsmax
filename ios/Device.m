da82167fc9649f07a42f071f7c52bf2599a29344
JD <juandiego89@hotmail.com>
2019-01-29 17:54:54 +0900

    add ios libraries and export method to check the device name

---
+++ ios/Device.m
@@ -0,0 +1,9 @@
+//
+//  Device.m
+//  RNReactNativeIphoneXXrXsXsmax
+//
+//  Created by Juan Diego Urena Vasquez on 1/29/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/Device.swift
@@ -0,0 +1,9 @@
+//
+//  Device.swift
+//  RNReactNativeIphoneXXrXsXsmax
+//
+//  Created by Juan Diego Urena Vasquez on 1/29/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
